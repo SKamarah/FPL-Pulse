95d71b39adf66c71aba127c81ad585a47657d20e
saleemk <k98.saleem@gmail.com>
2026-02-25 19:29:02 -0500

    initial commit

---
+++ src/main/java/com/Skamarah/FplProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Skamarah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplProjectApplication.class, args);
+	}
+
+}
